11e84bf87af49aec6adb4e6f4e91cf374745d956
Ngọc Thắng <89723279+thangdevweb@users.noreply.github.com>
2023-12-28 12:47:07 +0700

    Update page.tsx

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -2,6 +2,7 @@ import { Suspense } from 'react';
 import SpinnerLoading from '~/components/Loading/SpinnerLoading';
 import SearchResult from '~/components/SearchResult/SearchResult';
 
+export const runtime = "edge";
 export default function SearchPages({ searchParams }: { searchParams: { [key: string]: string } }) {
   return (
     <div className="min-h-[100vh] lg:max-w-[960px] xl:max-w-6xl mx-5 md:mx-8 xl:mx-auto lg:mt-[100px]">
